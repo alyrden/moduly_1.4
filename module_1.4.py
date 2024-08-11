bee629fbc176eb174cb3ee1cff9abcd1ae4b475d
alyrden <f50knox@gmail.com>
2024-08-11 23:18:22 +0300

    Add files via upload

---
+++ module_1.4.py
@@ -0,0 +1,10 @@
+#1 Не ленитесь написать все ответы.
+my_string = input("Введи число: ")
+#2
+my_string = input("Привет! Как тебя зовут? ".upper())
+my_string = input("Привет! Как тебя зовут? ".lower())
+my_string = input("Привет! Как тебя зовут? ".replace(" ", ""))
+#3
+my_string = "Привет! Как тебя зовут?"
+print(my_string[0])
+print(my_string[-1])
