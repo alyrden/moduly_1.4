23584b442b9ab72ce3114b888b32cc4b651de7b0
alyrden <f50knox@gmail.com>
2024-08-13 14:48:00 +0300

    Update module_1.4.py

--- module_1.4.py
+++ module_1.4.py
@@ -1,9 +1,13 @@
 #1 Не ленитесь написать все ответы.
 my_string = input("Введи число: ")
+print(len(my_string))
 #2
-my_string = input("Привет! Как тебя зовут? ".upper())
-my_string = input("Привет! Как тебя зовут? ".lower())
-my_string = input("Привет! Как тебя зовут? ".replace(" ", ""))
+my_string = input("Привет! Как тебя зовут? ")
+print(my_string.upper())
+my_string = input("Привет! Как тебя зовут? ")
+print(my_string.lower())
+my_string = input("Напиши 3 слова. ")
+print(my_string.replace(' ', ''))
 #3
 my_string = "Привет! Как тебя зовут?"
 print(my_string[0])
